99b960b8b35c839af5d207d6298b79d3b238cd0f
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2023-12-18 12:17:21 +0530

    2.0.1

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.get("/image", async (req, res) => {
   const query = req.query;
