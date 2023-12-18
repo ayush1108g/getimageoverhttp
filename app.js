248fbb29b92cf4ba033439e2d3347347cfa9e8f5
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2023-12-18 11:51:24 +0530

    1.0.1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.get("/image", async (req, res) => {
+  const query = req.query;
+  const imglink = Object.keys(query)[0];
+
+  try {
+    const response = await axios.get(imglink, { responseType: "arraybuffer" });
+
+    res.setHeader("Content-Type", response.headers["content-type"]);
+    res.send(Buffer.from(response.data, "binary"));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = app;
